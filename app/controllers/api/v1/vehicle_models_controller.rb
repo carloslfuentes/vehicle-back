8f63469d4ee9f8574661693a907b49ac99ddded9
Carlos Fuentes <carlos.fuentes@interfactura.com>
2021-03-23 20:06:58 -0600

    add changes

--- app/controllers/api/v1/vehicle_models_controller.rb
+++ app/controllers/api/v1/vehicle_models_controller.rb
@@ -2,6 +2,11 @@ class Api::V1::VehicleModelsController < Api::GenericController
 
   private
 
+  def filter(resources)
+    resources = resources.by_brand(params[:brand_name]) if params[:brand_name].present?
+    resources
+  end
+
   def permitted_params
     params.permit([:name, :brand])
   end
